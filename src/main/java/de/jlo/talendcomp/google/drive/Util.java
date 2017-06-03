d9bbdd6f2f2e2ec934374bfdabdc69d42957be33
Jan Lolling <jan.lolling@gmail.com>
2017-06-03 11:45:00 +0200

    package renamed and guave lib updated to make it compatible with AdWords component

--- src/main/java/de/jlo/talendcomp/google/drive/Util.java
+++ src/main/java/de/jlo/talendcomp/google/drive/Util.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.cimt.talendcomp.google.drive;
+package de.jlo.talendcomp.google.drive;
 
 import java.io.IOException;
 import java.util.List;
